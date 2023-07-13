d61c4bd84e744358db4601514a16cd248074a678
Alex Kinstler <akinstler@wincraft.com>
2023-07-13 16:03:26 -0500

    tt

--- .github/actions/hello/dist/index.js
+++ .github/actions/hello/dist/index.js
@@ -1225,13 +1225,23 @@ const github = __webpack_require__(469);
 try {
   //   throw new Error("Some error message");
 
+  core.debug("debug message");
+  core.warining("warning message");
+  core.error("Error message");
+
   const name = core.getInput("who-to-greet");
-  console.log("Hello ${name}");
+  console.log("Hello " + name);
+
+  core.setSecret(name);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("logging object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("hello", "Hello");
 } catch (error) {
   core.setFailed(error.message);
   //
--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -4,13 +4,23 @@ const github = require("@actions/github");
 try {
   //   throw new Error("Some error message");
 
+  core.debug("debug message");
+  core.warining("warning message");
+  core.error("Error message");
+
   const name = core.getInput("who-to-greet");
-  console.log("Hello ${name}");
+  console.log("Hello " + name);
+
+  core.setSecret(name);
 
   const time = new Date();
   core.setOutput("time", time.toTimeString());
 
+  core.startGroup("logging object");
   console.log(JSON.stringify(github, null, "\t"));
+  core.endGroup();
+
+  core.exportVariable("hello", "Hello");
 } catch (error) {
   core.setFailed(error.message);
   //
